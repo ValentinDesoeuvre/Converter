b2df879030c8f48b384ae83c32c8d834b98d1b6a
Val <pankakekaribou@hotmail.fr>
2023-10-03 13:09:31 +0200

    feat(Currencies): static component

--- src/components/Converter/Currencies/Currencies.tsx
+++ src/components/Converter/Currencies/Currencies.tsx
@@ -2,8 +2,13 @@ import './Currencies.scss';
 
 function Currencies() {
   return (
-    <div className="Currencies">
-      Currencies
+    <div className="currencies">
+      <h2 className='currencies-title'>Currencies</h2>
+      <ul className="currencies-list">
+        <li className="currencies-list-item">Currency 1</li>
+        <li className="currencies-list-item">Currency 2</li>
+        <li className="currencies-list-item">Currency 3</li>
+      </ul>
     </div>
   );
 }
